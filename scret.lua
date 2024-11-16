c883f85bb83869eb2f9d479b37a3e0e96a43febb
aslan90101 <aslan0aslanov0ezz@gmail.com>
2024-11-16 14:42:15 +0300

    Update and rename scrept.lua to scret.lua

--- scret.lua
+++ scret.lua
@@ -105,7 +105,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/
 })
  
 local Button = ScriptsSection:CreateButton({
-   Name = "CRTL+click tp❤️",
+   Name = "CTRL+click tp❤️",
    Callback = function()
 loadstring(game:HttpGet("https://github.com/aslan90101/click-tp/blob/main/TP.lua", true))()
         end,
